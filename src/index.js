e46506791f284da2120e60a841ebd70f01db6865
ushaveguru <usha.vadrevu1292@gmail.com>
2018-04-07 05:05:55 -0700

    First Commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb://127.0.0.1:27017',function(err,db){
-	if(err){
-		throw err;
-	}else{
-		console.log("Its connected");
-	}
-	db.close();
-})
